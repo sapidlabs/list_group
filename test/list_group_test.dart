fd492fbeb7ed5ed4c9812dc5904023a0c926721d
Abhishek <abhishek@sapidlabs.com>
2019-09-02 14:43:27 +0530

    Remove tests.

--- test/list_group_test.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:flutter_test/flutter_test.dart';
-
-// import 'package:list_group/list_group.dart';
-
-// void main() {
-//   test('adds one to input values', () {
-    
-//   });
-// }
